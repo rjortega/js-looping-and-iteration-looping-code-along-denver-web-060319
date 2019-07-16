5b7946cd8ff9d2d80d8168830746851929258195
Rj <rikrdo.ortega@gmail.com>
2019-07-16 01:22:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return array
+}
+
+function countdown(number) {
+    let counter = number
+    while (counter >= 0) {
+        console.log(counter)
+        counter--
+    }
+}
